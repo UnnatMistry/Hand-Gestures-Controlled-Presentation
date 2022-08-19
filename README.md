67a579729c31909b6fd1fd409529d59fbf0f77fb
Unnat Mistry <87070570+UnnatMistry@users.noreply.github.com>
2022-08-19 20:49:02 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,35 @@
+
+# Hand Gestures Controlled Presentation
+
+Developed using Python and OpenCV, this program can be used to control presentation and perform annotations on slides.
+
+
+## Features
+
+| Operation             | Gesture                                                                |
+| ----------------- | ------------------------------------------------------------------ |
+| Show Pointer | Index and middle finger movement |
+| Annotation | Index finger movement |
+| Remove Annotation | Show 3 fingers \| Index, middle and ring finger|
+| Previous Slide |Show thumb |
+| Next Slide | Show pinky finger |
+
+
+
+
+## Execution
+
+1. Install Python
+2. Run cmd
+
+```bash
+  pip install -r requirements.txt
+```
+&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; OR
+```
+  pip3 install -r requirements.txt
+```
+
+3. Rename your ppt to presentation.pptx and copy it to the code folder.
+4. Run the code.
+
---
+++ handGesturesControlledPresentation.py
@@ -0,0 +1,159 @@
+from turtle import screensize
+import aspose.pydrawing as drawing
+from cvzone.HandTrackingModule import HandDetector
+import cv2
+import os
+import numpy as np
+import shutil
+
+def extractImgFromPPT():
+    import aspose.slides as slides
+    if os.path.isdir("Presentation"):
+        # os.rmdir("Presentation")
+        shutil.rmtree("Presentation")
+    os.mkdir("Presentation")
+    # Load presentation
+    pres = slides.Presentation("presentation.pptx")
+
+    # Loop through slides
+    for index in range(pres.slides.length):
+        # Get reference of slide
+        slide = pres.slides[index]
+
+        # Define custom size
+        # size = drawing.Size(1280, 720)
+        size = drawing.Size(1540,779)
+
+        # Save as PNG
+        slide.get_thumbnail(size).save(
+            "./Presentation/{i}.png".format(i=index), drawing.imaging.ImageFormat.png)
+
+def handGestures():
+    # Parameters
+    import PIL
+    image = PIL.Image.open("presentation/1.png")
+    width, height = image.size
+    
+    width, height = 1540, 779
+    # width, height = 1920, 1080
+
+    gestureThreshold = 300
+    folderPath = "Presentation"
+
+    # Camera Setup
+    cap = cv2.VideoCapture(0)
+    cap.set(3, width)
+    cap.set(4, height)
+
+    # Hand Detector
+    detectorHand = HandDetector(detectionCon=0.5, maxHands=1)
+
+    # Variables
+    imgList = []
+    delay = 12
+    buttonPressed = False
+    counter = 0
+    drawMode = False
+    imgNumber = 0
+    delayCounter = 0
+    annotations = [[]]
+    annotationNumber = -1
+    annotationStart = False
+    hs, ws = int(120 * 1), int(213 * 1)  # width and height of small image
+
+    # Get list of presentation images
+    pathImages = sorted(os.listdir(folderPath), key=len)
+    print(pathImages)
+
+    while True:
+        # Get image frame
+        success, img = cap.read()
+        img = cv2.flip(img, 1)
+        pathFullImage = os.path.join(folderPath, pathImages[imgNumber])
+        imgCurrent = cv2.imread(pathFullImage)
+        imgCurrent = cv2.resize(imgCurrent,(width,height))
+        # Find the hand and its landmarks
+        hands, img = detectorHand.findHands(img)  # with draw
+        # Draw Gesture Threshold line
+        cv2.line(img, (0, gestureThreshold),
+                (width, gestureThreshold), (0, 255, 0), 10)
+
+        if hands and buttonPressed is False:  # If hand is detected
+
+            hand = hands[0]
+            cx, cy = hand["center"]
+            lmList = hand["lmList"]  # List of 21 Landmark points
+            fingers = detectorHand.fingersUp(hand)  # List of which fingers are up
+
+            # Constrain values for easier drawing
+            xVal = int(np.interp(lmList[8][0], [width // 2, width//1.5], [-30, width+30]))
+            yVal = int(np.interp(lmList[8][1], [150, height-250], [-30, height+30]))
+            indexFinger = xVal, yVal
+
+            if cy <= gestureThreshold:  # If hand is at the height of the face
+                if fingers == [1, 0, 0, 0, 0]:
+                    print("Left")
+                    buttonPressed = True
+                    if imgNumber > 0:
+                        imgNumber -= 1
+                        annotations = [[]]
+                        annotationNumber = -1
+                        annotationStart = False
+                if fingers == [0, 0, 0, 0, 1]:
+                    print("Right")
+                    buttonPressed = True
+                    if imgNumber < len(pathImages) - 1:
+                        imgNumber += 1
+                        annotations = [[]]
+                        annotationNumber = -1
+                        annotationStart = False
+
+            if fingers == [0, 1, 1, 0, 0]:
+                cv2.circle(imgCurrent, indexFinger, 12, (0, 0, 255), cv2.FILLED)
+
+            if fingers == [0, 1, 0, 0, 0]:
+                if annotationStart is False:
+                    annotationStart = True
+                    annotationNumber += 1
+                    annotations.append([])
+                print(annotationNumber)
+                annotations[annotationNumber].append(indexFinger)
+                cv2.circle(imgCurrent, indexFinger, 12, (0, 0, 255), cv2.FILLED)
+
+            else:
+                annotationStart = False
+
+            if fingers == [0, 1, 1, 1, 0]:
+                if annotations:
+                    annotations.pop(-1)
+                    annotationNumber -= 1
+                    buttonPressed = True
+
+        else:
+            annotationStart = False
+
+        if buttonPressed:
+            counter += 1
+            if counter > delay:
+                counter = 0
+                buttonPressed = False
+
+        for i, annotation in enumerate(annotations):
+            for j in range(len(annotation)):
+                if j != 0:
+                    cv2.line(imgCurrent, annotation[j - 1],
+                            annotation[j], (0, 0, 200), 12)
+
+        imgSmall = cv2.resize(img, (ws, hs))
+        h, w, _ = imgCurrent.shape
+        imgCurrent[0:hs, w - ws: w] = imgSmall
+
+        cv2.imshow("Slides", imgCurrent)
+        # cv2.imshow("Image", img)
+
+        key = cv2.waitKey(1)
+        if key == ord('q'):
+            break
+if __name__=='__main__':
+    extractImgFromPPT()
+    handGestures()
